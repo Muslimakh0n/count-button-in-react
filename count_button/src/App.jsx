3e474553b6f6438160537bfd7512f035cd234893
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-07-30 02:09:58 +0500

    third commit

--- count_button/src/App.jsx
+++ count_button/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); //useState o'zida 0ni saqlab turibti, button bosilganda esa component change bovotti
   return (
     <>
-      <h1>Count : {count}</h1>
+      <h1 bg-color="blue">Count : {count}</h1>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(count + 1)}>+1</button>
     </>
