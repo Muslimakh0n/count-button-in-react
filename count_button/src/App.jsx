d57c793d16431e4a26d957db0d7ff331abec2743
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-07-30 00:41:09 +0500

    second commit

--- count_button/src/App.jsx
+++ count_button/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Nothiiiing tho</h1>
+      <h1>Count : {count}</h1>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </>
   );
 }
