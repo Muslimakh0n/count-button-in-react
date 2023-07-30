df6c904e99683f1561046113d588d24f6224468c
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-07-30 05:31:00 +0500

    last commit

--- count_button/src/App.jsx
+++ count_button/src/App.jsx
@@ -3,11 +3,42 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0); //useState o'zida 0ni saqlab turibti, button bosilganda esa component change bovotti
+  let colour;
+  if (count == 0) {
+    colour = "yellow";
+  } else if (count > 0) {
+    colour = "green";
+  } else {
+    colour = "purple";
+  }
+  const styles = {
+    color: colour,
+  };
   return (
     <>
-      <h1 bg-color="blue">Count : {count}</h1>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <div className=" mx-auto flex h-screen w-screen items-center justify-center bg-[#252425]">
+        <div className="w-[500px] bg-[#252425] p-4">
+          {/* // o'zing biror tailwindda yoq qiymatni bermoqchi bolsang arrayning
+          //ichida yozasan! */}
+          <h1 className=" text-center text-4xl text-white" style={styles}>
+            Count : {count}
+          </h1>
+          <div className="mt-4 text-center text-white">
+            <button
+              className=" text      ml-3 rounded border border-transparent bg-black px-4 py-1  text-2xl shadow hover:border"
+              onClick={() => setCount(count - 1)}
+            >
+              -1
+            </button>
+            <button
+              className="ml-3 rounded border border-transparent bg-black px-4 py-1 text-2xl text-white shadow hover:border"
+              onClick={() => setCount(count + 1)}
+            >
+              +1
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
